3bd7b0d262cb1b1dbe68beaa8b13fe6f2d4b7235
Tatiana Liabina <t.liabina@icloud.com>
2022-12-01 15:39:12 +0400

    Add tasks

---
+++ Home_work_6_1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 4
+
+Console.WriteLine("Сколько чисел хотите ввести? ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int[] array = new int[n];
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(string.Join(",", array));
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) count++;
+}
+
+Console.WriteLine("Спасибо, вводить числа больше не нужно.");
+Console.WriteLine("Число введенных чисел больше 0: " + count);
+
+
+
+
+
